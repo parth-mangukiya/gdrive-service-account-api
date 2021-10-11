58de439bb8108a8a658c3f0c4dcd6b8c64d0a318
justcode infotech <64396707+justcodeinfotech@users.noreply.github.com>
2021-10-11 16:20:31 +0530

    Update example.php

--- example.php
+++ example.php
@@ -4,18 +4,17 @@ define('DRIVE_TYPE', 'shared'); // there are 2 type of drive 'own' , 'shared'  S
 define('DRIVE_ID', '0AKmdVV3sQwP-Uk9PVA'); // this is The Shared drive id where magic happen
 define('FOLDER_ID', '1yriwrn4BWYhSxTP-IVf7nof_nO-XG1bt'); // this is the folder id in which all megic happen
 
-// require_once './action.php';
-require_once './driveHelperWithoutSymfony.php';
-$googleDrive = new GoogledriveHelper;
+require_once './action.php';
+$googleDrive = new googleDrive;
 
 $folderPath = 'test child'; // this is the folder path in which you wants to upoad
 
 # get list of files
-// $list_of_files = $googleDrive->getlist($folderPath); // return Folder and flies list
+$list_of_files = $googleDrive->getlist($folderPath); // return Folder and flies list
 
 
 # create a folder
-// $googleDrive->createFolder($folderPath);
+$googleDrive->createFolder($folderPath);
 
 
 // #  upload file to google drive D
@@ -25,7 +24,7 @@ $filePath = array(
     'D:/xampp/htdocs/drive2/download.jpg',
     'D:/xampp/htdocs/drive2/dummy.pdf',
 ); // file path that you wants to upload in google drive please use in array
-$upload = $googleDrive->uploadFilesToFolder('', $filePath);
+$upload = $googleDrive->uploadFile($folderPath, $filePath);
 
 # download the google drive
-// $download = $googleDrive->download($folderPath);
+$download = $googleDrive->download($folderPath);
